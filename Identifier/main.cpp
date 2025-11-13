f7101ed401246dd039b97d7793dcae072867fb90
Jarin Aohona <jarinaohona7@gmail.com>
2025-11-13 13:20:48 +0600

    Update main.cpp

--- Identifier/main.cpp
+++ Identifier/main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <fstream>
 using namespace std;
-
 int main() {
     ifstream file("sample.txt");
     string word;
@@ -10,31 +9,21 @@ int main() {
         cout << "Error: Could not open sample.txt file!" << endl;
         return 0;
     }
-
     cout << "Checking identifiers from sample.txt:\n\n";
-
     while (file >> word) {
-
-
+        
         int len = word.length();
         if (word[len - 1] == ';')
             word = word.substr(0, len - 1);
-
-        // skip keywords
         if (word == "int" || word == "float" || word == "double" ||
             word == "char" || word == "string")
             continue;
-
         bool valid = true;
-
-
         if (!((word[0] >= 'A' && word[0] <= 'Z') ||
               (word[0] >= 'a' && word[0] <= 'z') ||
                word[0] == '_')) {
             valid = false;
         }
-
-
         for (int i = 1; i < word.length(); i++) {
             char ch = word[i];
             if (!((ch >= 'A' && ch <= 'Z') ||
@@ -45,13 +34,11 @@ int main() {
                 break;
             }
         }
-
         if (valid)
             cout << word << " ---> Valid Identifier " << endl;
         else
             cout << word << " ---> Invalid Identifier " << endl;
     }
-
     file.close();
     return 0;
 }
